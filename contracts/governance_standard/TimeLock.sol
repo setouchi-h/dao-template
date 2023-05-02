334424dc1e2c55cbebf8ce31e55073b68a7cb6ac
setouchi-h <setouchi.kazuki@gmail.com>
2023-05-02 23:16:43 +0900

    initial commit

---
+++ contracts/governance_standard/TimeLock.sol
@@ -0,0 +1,10 @@
+// We want to wait for a new vote to be "executed"
+
+// Everyone who holds the governance token has to pay 5 tokens
+
+// Give time to users to "get out" if they don't like a governance
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
